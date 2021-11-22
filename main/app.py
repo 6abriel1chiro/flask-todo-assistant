3a7cc7bf7077683f4a04dacc05f5bbc6a9db3463
Gabriel Ichiro <gabriel582084@gmail.com>
2021-11-22 08:11:24 -0400

    adden login. home. about, signuop
    psiosisiq

--- main/app.py
+++ main/app.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template , url_for, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/")
+def home():
+        return render_template('home.html')
 
+@app.route("/login", methods=['GET','POST'])
 def login():
         return render_template('login.html')
